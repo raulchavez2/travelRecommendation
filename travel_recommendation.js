b659b802477e76b442a7867455da5b81d8660196
raulchavez2 <curlycarlosmonroy@gmail.com>
2025-01-29 06:51:26 -0500

    finished

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,4 +1,5 @@
 const btnSearch = document.getElementById('btnSearch');
+const btnClear = document.getElementById('btnClear');
 
 function searchCondition() {
     const input = document.getElementById('conditionInput').value.toLowerCase();
@@ -9,25 +10,51 @@ function searchCondition() {
       .then(response => response.json())
       .then(data => {
         const item = data[Object.keys(data).find(item => item.toLowerCase().includes(input))];
-        console.log(item)
-
         
         if (item) {
-          const name1 = item[0].name;
-          const imageUrl1 = item[0].imageUrl;
-          const description1 = item[0].description;
+            let name1;
+            let imageUrl1;
+            let description1;
+            let name2;
+            let imageUrl2;
+            let description2;
+            let time1
+            let time2
+            if(item[0].name != "Australia"){  
+                const options1 = { timeZone: `${item[0].timeZone}`, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                time1 = new Date().toLocaleTimeString('en-US', options1);
+                const options2 = { timeZone: item[1].timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                time2 = new Date().toLocaleTimeString('en-US', options2);
+                name1 = item[0].name;
+                imageUrl1 = item[0].imageUrl;
+                description1 = item[0].description;
 
-          const name2 = item[1].name;
-          const imageUrl2 = item[1].imageUrl;
-          const description2 = item[1].description;
+                name2 = item[1].name;
+                imageUrl2 = item[1].imageUrl;
+                description2 = item[1].description;
+            } else{const options1 = { timeZone: item[0].cities[0].timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                time1 = new Date().toLocaleTimeString('en-US', options1);
+                const options2 = { timeZone: item[1].cities[0].timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+                time2 = new Date().toLocaleTimeString('en-US', options2);
+                
+                name1 = item[0].cities[0].name;
+                imageUrl1 = item[0].cities[0].imageUrl;
+                description1 = item[0].cities[0].description;
 
-          resultDiv.innerHTML += `<img src="${name1}" alt="hjh">`;
-          resultDiv.innerHTML += `<h2>${imageUrl1}</h2>`;          
+                name2 = item[1].cities[0].name;
+                imageUrl2 = item[1].cities[0].imageUrl;
+                description2 = item[1].cities[0].description;
+            }
+
+          resultDiv.innerHTML += `<img src="${imageUrl1}" alt="hjh">`;
+          resultDiv.innerHTML += `<h2>${name1}</h2>`;          
           resultDiv.innerHTML += `<p>${description1}</p>`;
-          resultDiv.innerHTML += `</br></br></br>`
-          resultDiv.innerHTML += `<img src="${name2}" alt="hjh">`;
-          resultDiv.innerHTML += `<h2>${imageUrl2}</h2>`;          
+          resultDiv.innerHTML += `<p>Current time: ${time1}</p>`;
+          resultDiv.innerHTML += `</br></br>=`
+          resultDiv.innerHTML += `<img src="${imageUrl2}" alt="hjh">`;
+          resultDiv.innerHTML += `<h2>${name2}</h2>`;          
           resultDiv.innerHTML += `<p>${description2}</p>`;
+          resultDiv.innerHTML += `<p>Current Time: ${time2}</p>`;
 
         } else {
           resultDiv.innerHTML = 'Condition not found.';
@@ -37,4 +64,9 @@ function searchCondition() {
         console.error('Error:', error);
       });
   }
-    btnSearch.addEventListener('click', searchCondition);
+  btnSearch.addEventListener('click', searchCondition);
+  btnClear.addEventListener('click', clearCondition);
+
+  function clearCondition(){
+    document.getElementById('result').innerHTML = ""
+  }
